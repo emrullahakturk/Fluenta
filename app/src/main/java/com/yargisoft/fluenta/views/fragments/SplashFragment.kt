9231182489c474a6fa4e738e172d1fdd3193dc97
unknown <akturk.emrullah@hotmail.com>
2024-11-01 19:54:37 +0300

    Add: Splash Fragment & Data Binding

---
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/SplashFragment.kt
@@ -0,0 +1,27 @@
+package com.yargisoft.fluenta.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.yargisoft.fluenta.R
+import com.yargisoft.fluenta.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding : FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_splash,container,false)
+
+       return binding.root
+
+    }
+}
