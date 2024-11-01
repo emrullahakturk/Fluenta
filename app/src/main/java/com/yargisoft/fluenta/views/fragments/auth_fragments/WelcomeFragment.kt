b9a3e3f0aeb3f080bd30533c686e53e4a9815f58
unknown <akturk.emrullah@hotmail.com>
2024-11-02 00:13:01 +0300

    Add: Welcome Page Added

---
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/WelcomeFragment.kt
@@ -0,0 +1,30 @@
+package com.yargisoft.fluenta.views.fragments.auth_fragments
+
+import android.graphics.Paint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.yargisoft.fluenta.R
+import com.yargisoft.fluenta.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+    private lateinit var binding :FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_welcome, container, false)
+
+        //Giriş yap textini altı çizili konuma getirdik
+        binding.loginTv.paintFlags =  Paint.UNDERLINE_TEXT_FLAG
+
+
+        return binding.root
+    }
+
+}
