89e407b7765aefc8622c6c66bfc0440eecddffd1
unknown <akturk.emrullah@hotmail.com>
2024-11-02 19:54:52 +0300

    Add: Navigations Added to PasswordFragment Class

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/PasswordFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/PasswordFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.yargisoft.fluenta.R
+import com.yargisoft.fluenta.databinding.FragmentPasswordBinding
 
 
 class PasswordFragment : Fragment() {
 
+    private lateinit var binding : FragmentPasswordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password, container, false)
+    ): View{
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_password, container, false)
+
+        binding.forgotPasswordTv.setOnClickListener {
+            findNavController().navigate(R.id.passwordToResetPassword)
+        }
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.passwordToQuote)
+        }
+
+        return binding.root
     }
 
 
