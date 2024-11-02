7f207b13940dd44d9aadbab97cdf93a9c31f6154
unknown <akturk.emrullah@hotmail.com>
2024-11-02 19:39:51 +0300

    Add: Navigations Added to LoginFragment Class

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/LoginFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/LoginFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.yargisoft.fluenta.R
+import com.yargisoft.fluenta.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.loginToPassword)
+        }
+        binding.signUpTv.setOnClickListener {
+            findNavController().navigate(R.id.loginToSignUp)
+        }
+
+        return binding.root
     }
 
 
