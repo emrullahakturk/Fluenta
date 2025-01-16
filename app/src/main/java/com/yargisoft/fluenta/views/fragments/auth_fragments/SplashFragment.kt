cff567dd4b53fcc70e3594802c2264d023925e48
unknown <akturk.emrullah@hotmail.com>
2025-01-16 20:43:50 +0300

    Update: Splash Fragment Modified: Checking if the user is logged in

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/SplashFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/SplashFragment.kt
@@ -1,16 +1,21 @@
 package com.yargisoft.fluenta.views.fragments.auth_fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.yargisoft.fluenta.R
 import com.yargisoft.fluenta.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-
-class SplashFragment : Fragment() {
+@AndroidEntryPoint
+class SplashFragment  @Inject constructor(): Fragment() {
 
     private lateinit var binding: FragmentSplashBinding
 
@@ -28,4 +33,27 @@ class SplashFragment : Fragment() {
         return binding.root
 
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Splash ekranı 2 saniye gösterelim (isteğe bağlı)
+        Handler(Looper.getMainLooper()).postDelayed({
+            checkUserSession()
+        }, 2000)
+    }
+
+    private fun checkUserSession() {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            // Kullanıcı oturum açmış, Home sayfasına yönlendir
+            findNavController().navigate(R.id.splashToQuote)
+        } else {
+            // Kullanıcı oturum açmamış, FirstPage'e yönlendir
+            findNavController().navigate(R.id.splashToWelcome)
+        }
+    }
 }
+
+
+
