838d33865b429c5e06f6248ccbe75305bb4a0d25
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-01-18 01:13:34 +0300

    Update: QuotesFragment modified

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/QuoteFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/auth_fragments/QuoteFragment.kt
@@ -5,20 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.yargisoft.fluenta.databinding.FragmentQuoteBinding
+import com.yargisoft.fluenta.viewmodel.QuoteViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class QuoteFragment : Fragment() {
 
-    private lateinit var binding : FragmentQuoteBinding
+
+    private var _binding: FragmentQuoteBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: QuoteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        binding = FragmentQuoteBinding.inflate(inflater,container,false)
-
+        _binding = FragmentQuoteBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view,savedInstanceState)
+
+        // Rastgele bir söz yükle
+        viewModel.loadRandomQuote()
+
+        // Sözleri gözlemle
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.quote.collect { quote ->
+                    if (quote != null) {
+                        binding.authorTv.text = quote.author
+                        binding.quoteTextView.text = quote.quote
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
