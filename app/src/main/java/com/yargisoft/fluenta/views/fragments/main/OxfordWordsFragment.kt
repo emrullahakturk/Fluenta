dc0636f246c18d364c8db87ae11ff88c496a9460
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-01-20 22:25:30 +0300

    Add & Update: Dice Lottie file added. OxfordWordsFragment updated. New words are randomly selected via dice and reflected on the screen.

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/main/OxfordWordsFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/main/OxfordWordsFragment.kt
@@ -11,32 +11,80 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.yargisoft.fluenta.R
+import com.yargisoft.fluenta.databinding.FragmentOxfordWordsBinding
 import com.yargisoft.fluenta.viewmodel.OxfordWordViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.text.Typography.quote
 
 @AndroidEntryPoint
 class OxfordWordsFragment : Fragment() {
 
     private val viewModel: OxfordWordViewModel  by viewModels()
-
+    private lateinit var _binding: FragmentOxfordWordsBinding
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View{
+
+        _binding = FragmentOxfordWordsBinding.inflate(inflater, container, false)
 
         viewModel.loadRandomQuote()
-        // Sözleri gözlemle
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.word.collect { quote ->
-                   Log.d("QuoteFragment", "Quote: $quote")
+                viewModel.word.collect { word ->
+                    binding.apply {
+                        tvWord.text = word?.word
+                        tvType.text = word?.type
+                        tvLevel.text = word?.level
+                        tvMeaning.text = word?.meaning
+                        tvEnExample.text = word?.enExample
+                        tvTrExample.text = word?.trExample
+                    }
                 }
             }
         }
 
-        return inflater.inflate(R.layout.fragment_oxford_words, container, false)
+
+
+        binding.diceLottie.setOnClickListener {
+            if (!binding.diceLottie.isAnimating) {
+                binding.diceLottie.playAnimation()
+                CoroutineScope(Dispatchers.Main).launch {
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+                    viewModel.loadRandomQuote()
+                    delay(200)
+
+                }
+
+            }
+        }
+
+        return binding.root
     }
 
  }
