95a3cabf3e6e44385606e0fecc24c323fc9ec22a
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-01-30 16:29:59 +0300

    Update: Database initializer added. CommonPhraseInitializer method added.

--- app/src/main/java/com/yargisoft/fluenta/data/model/DatabaseInitializer.kt
+++ app/src/main/java/com/yargisoft/fluenta/data/model/DatabaseInitializer.kt
@@ -8,24 +8,39 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
-class DatabaseInitializer @Inject constructor(private val context: Context,
-                                              private val quoteDao: QuoteDao,
-                                              private val oxfordWordDao: OxfordWordDao,
-                                              private val mostCommonWordDao: MostCommonWordDao) {
+class DatabaseInitializer @Inject constructor(
+    private val context: Context,
+    private val quoteDao: QuoteDao,
+    private val oxfordWordDao: OxfordWordDao,
+    private val mostCommonWordDao: MostCommonWordDao,
+    private val commonPhraseDao: CommonPhraseDao,
+    ) {
 
     suspend fun quoteInitializer() = withContext(Dispatchers.IO) {
         val json = context.assets.open("quotes.json").bufferedReader().use { it.readText() }
         val quotes: List<Quote> = Gson().fromJson(json, object : TypeToken<List<Quote>>() {}.type)
         quoteDao.insertAll(quotes)
     }
+
     suspend fun oxfordWordInitializer() = withContext(Dispatchers.IO) {
         val json = context.assets.open("oxford_words.json").bufferedReader().use { it.readText() }
-        val word: List<OxfordWord> = Gson().fromJson(json, object : TypeToken<List<OxfordWord>>() {}.type)
+        val word: List<OxfordWord> =
+            Gson().fromJson(json, object : TypeToken<List<OxfordWord>>() {}.type)
         oxfordWordDao.insertAll(word)
     }
+
     suspend fun mostCommonWordInitializer() = withContext(Dispatchers.IO) {
-        val json = context.assets.open("most_common_c1_c2.json").bufferedReader().use { it.readText() }
-        val word: List<MostCommonWord> = Gson().fromJson(json, object : TypeToken<List<MostCommonWord>>() {}.type)
+        val json =
+            context.assets.open("most_common_c1_c2.json").bufferedReader().use { it.readText() }
+        val word: List<MostCommonWord> =
+            Gson().fromJson(json, object : TypeToken<List<MostCommonWord>>() {}.type)
         mostCommonWordDao.insertAll(word)
+    }
+
+    suspend fun commonPhraseInitializer() = withContext(Dispatchers.IO) {
+        val json = context.assets.open("common_phrases.json").bufferedReader().use { it.readText() }
+        val phrase: List<CommonPhrase> =
+            Gson().fromJson(json, object : TypeToken<List<CommonPhrase>>() {}.type)
+        commonPhraseDao.insertAll(phrase)
     }
 }
