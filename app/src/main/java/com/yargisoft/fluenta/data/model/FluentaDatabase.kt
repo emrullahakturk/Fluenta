2e38b736db39b5e38dc66951da88362f50c16c2a
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-01-30 16:36:33 +0300

    Update: Fluenta database updated.

--- app/src/main/java/com/yargisoft/fluenta/data/model/FluentaDatabase.kt
+++ app/src/main/java/com/yargisoft/fluenta/data/model/FluentaDatabase.kt
@@ -3,9 +3,10 @@ package com.yargisoft.fluenta.data.model
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Quote::class, OxfordWord::class,MostCommonWord::class], version = 1, exportSchema = false)
+@Database(entities = [Quote::class, OxfordWord::class,MostCommonWord::class, CommonPhrase::class], version = 1, exportSchema = false)
 abstract class FluentaDatabase : RoomDatabase() {
     abstract fun quoteDao(): QuoteDao
     abstract fun oxfordWordDao(): OxfordWordDao
     abstract fun mostCommonWordDao(): MostCommonWordDao
+    abstract fun commonPhraseDao(): CommonPhraseDao
 }
