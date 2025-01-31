effee09c696ad8efd7e0bbadff9d3190df247236
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-01-31 14:34:39 +0300

    Update : TranslatorRepository updated.

--- app/src/main/java/com/yargisoft/fluenta/data/repository/TranslatorRepository.kt
+++ app/src/main/java/com/yargisoft/fluenta/data/repository/TranslatorRepository.kt
@@ -3,14 +3,24 @@ package com.yargisoft.fluenta.data.repository
 import com.yargisoft.fluenta.data.network.TranslateApiService
 import javax.inject.Inject
 
+
 class TranslatorRepository @Inject constructor(
     private val apiService: TranslateApiService
 ) {
     suspend fun translateText(text: String, sourceLang: String, targetLang: String): String {
         return try {
             val response = apiService.translate(sourceLang, targetLang, text)
-            val translatedText = (response[0] as List<*>)[0] as List<*>
-            translatedText[0].toString()
+
+            // Dönen cevabı düzeltme: Tüm çevirileri birleştiriyoruz.
+            val translatedText = StringBuilder()
+            val translatedSegments = response[0] as List<*>
+
+            for (segment in translatedSegments) {
+                val segmentList = segment as List<*>
+                translatedText.append(segmentList[0].toString()).append(" ") // Cümleleri birleştir
+            }
+
+            translatedText.toString().trim() // Fazla boşlukları temizle
         } catch (e: Exception) {
             "Çeviri yapılamadı: ${e.message}"
         }
