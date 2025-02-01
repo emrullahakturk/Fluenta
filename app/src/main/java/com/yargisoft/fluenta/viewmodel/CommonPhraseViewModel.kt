0c7d13985bf30bf08b3c8b7636197e04f3d69107
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-01 13:29:46 +0300

    Update: CommonPhraseViewModel updated, tts added to viewmodel

--- app/src/main/java/com/yargisoft/fluenta/viewmodel/CommonPhraseViewModel.kt
+++ app/src/main/java/com/yargisoft/fluenta/viewmodel/CommonPhraseViewModel.kt
@@ -1,9 +1,11 @@
 package com.yargisoft.fluenta.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yargisoft.fluenta.data.model.CommonPhrase
 import com.yargisoft.fluenta.data.repository.CommonPhraseRepository
+import com.yargisoft.fluenta.usecase.TextToSpeechUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,6 +19,8 @@ class CommonPhraseViewModel @Inject constructor() : ViewModel() {
     val phrase: StateFlow<CommonPhrase?> get() = _phrase
 
     @Inject
+    lateinit var textToSpeechUseCase: TextToSpeechUseCase
+    @Inject
     lateinit var commonPhraseRepository: CommonPhraseRepository
 
     fun loadRandomMostCommonWord() {
@@ -28,6 +32,19 @@ class CommonPhraseViewModel @Inject constructor() : ViewModel() {
                 _phrase.value = null
             }
         }
+    }
+
+
+    fun speak(text: String, context: Context) {
+        if (textToSpeechUseCase.isTextToSpeechEnabled(context)){
+            textToSpeechUseCase.speak(text)
+        }else{
+            textToSpeechUseCase.alertBuilderForTextToSpeech(context)
+        }
+    }
+
+    fun stop() {
+        textToSpeechUseCase.stop()
     }
 
 }
