f69ab86575a0476086f18c23621cab7ba9c76b2a
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-02 20:02:14 +0300

    Update: OxfordWordsFragment updated. OxfordWordsFragment xml updated. Favorite icon added to card view. When the user click the icon, word/phrase will be added to favorites database table.

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/main/OxfordWordsFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/main/OxfordWordsFragment.kt
@@ -1,38 +1,37 @@
 package com.yargisoft.fluenta.views.fragments.main
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.yargisoft.fluenta.R
 import com.yargisoft.fluenta.databinding.FragmentOxfordWordsBinding
+import com.yargisoft.fluenta.viewmodel.FavoriteViewModel
 import com.yargisoft.fluenta.viewmodel.OxfordWordViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.text.Typography.quote
 
 @AndroidEntryPoint
 class OxfordWordsFragment : Fragment() {
 
-    private val viewModel: OxfordWordViewModel  by viewModels()
+    private val viewModel: OxfordWordViewModel by viewModels()
+    private val favoriteViewModel: FavoriteViewModel by viewModels()
     private lateinit var _binding: FragmentOxfordWordsBinding
     private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
+    ): View {
 
         _binding = FragmentOxfordWordsBinding.inflate(inflater, container, false)
 
@@ -56,6 +55,7 @@ class OxfordWordsFragment : Fragment() {
 
 
         binding.diceLottie.setOnClickListener {
+            binding.favoriteIcon.isClickable = false
             binding.btnSpeak.isClickable = false
             viewModel.ttsStop()
             if (!binding.diceLottie.isAnimating) {
@@ -66,7 +66,19 @@ class OxfordWordsFragment : Fragment() {
                         delay(200)
                         viewModel.loadRandomOxfordWord()
                     }
+
+                    if (favoriteViewModel.isWordFavorite(
+                            binding.tvWord.text.toString(),
+                            "oxford_word"
+                        )
+                    ) {
+                        binding.favoriteIcon.setImageResource(R.drawable.ic_favorite_filled)
+                    } else {
+                        binding.favoriteIcon.setImageResource(R.drawable.ic_favorite)
+                    }
+
                     binding.btnSpeak.isClickable = true
+                    binding.favoriteIcon.isClickable = true
                 }
 
             }
@@ -77,7 +89,26 @@ class OxfordWordsFragment : Fragment() {
             viewModel.ttsSpeak(word, requireContext())
         }
 
+        binding.favoriteIcon.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                if (favoriteViewModel.isWordFavorite(
+                        binding.tvWord.text.toString(),
+                        "oxford_word"
+                    )
+                ) {
+                    Toast.makeText(requireContext(), "Favorite ", Toast.LENGTH_SHORT).show()
+
+                    favoriteViewModel.removeFavorite(binding.tvWord.text.toString(), "oxford_word")
+                    binding.favoriteIcon.setImageResource(R.drawable.ic_favorite)
+                } else {
+                    Toast.makeText(requireContext(), "Favorite not", Toast.LENGTH_SHORT).show()
+
+                    favoriteViewModel.addFavorite(binding.tvWord.text.toString(), "oxford_word")
+                    binding.favoriteIcon.setImageResource(R.drawable.ic_favorite_filled)
+                }
+            }
+        }
+
         return binding.root
     }
-
- }
+}
