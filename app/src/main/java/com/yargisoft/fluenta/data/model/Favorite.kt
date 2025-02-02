0aa0741a1fcb36a1e5b562cedb7e5a850746468a
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-02 19:54:20 +0300

    Add: Favorite model class added

--- app/src/main/java/com/yargisoft/fluenta/data/model/Favorite.kt
+++ app/src/main/java/com/yargisoft/fluenta/data/model/Favorite.kt
@@ -1,11 +1,12 @@
 package com.yargisoft.fluenta.data.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "favorites")
 data class Favorite(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val word: String,
-    val meaning: String, // Anlamını saklamak için
     val category: String // "Oxford3000", "C1C2", "Phrases" vb. //
 )
