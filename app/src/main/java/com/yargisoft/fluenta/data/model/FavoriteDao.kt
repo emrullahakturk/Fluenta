aca2e6bee291d9dafb62dee7a71b30d57f7106f2
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-02 19:55:26 +0300

    Add: Favorite Dao added

---
+++ app/src/main/java/com/yargisoft/fluenta/data/model/FavoriteDao.kt
@@ -0,0 +1,23 @@
+package com.yargisoft.fluenta.data.model
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteWordDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteWord(favoriteWord: Favorite)
+
+    @Query("DELETE FROM favorites WHERE word = :word AND category = :category")
+    suspend fun deleteFavoriteWord(word: String, category: String)
+
+    @Query("SELECT * FROM favorites WHERE category = :category")
+    fun getFavoritesByCategory(category: String): Flow<List<Favorite>>
+
+    @Query("SELECT EXISTS(SELECT * FROM favorites WHERE word = :word AND category = :category)")
+    suspend fun isWordFavorite(word: String, category: String): Boolean
+}
