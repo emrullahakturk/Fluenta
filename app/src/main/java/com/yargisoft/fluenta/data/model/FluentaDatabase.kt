2cf97e5a6f863af0a1e295c83c204bfa37dbf013
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-02 19:56:12 +0300

    Update: Fluenta database updated, dao function invoked

--- app/src/main/java/com/yargisoft/fluenta/data/model/FluentaDatabase.kt
+++ app/src/main/java/com/yargisoft/fluenta/data/model/FluentaDatabase.kt
@@ -3,10 +3,11 @@ package com.yargisoft.fluenta.data.model
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Quote::class, OxfordWord::class,MostCommonWord::class, CommonPhrase::class], version = 1, exportSchema = false)
+@Database(entities = [Quote::class, OxfordWord::class,MostCommonWord::class, CommonPhrase::class,Favorite::class], version = 1, exportSchema = false)
 abstract class FluentaDatabase : RoomDatabase() {
     abstract fun quoteDao(): QuoteDao
     abstract fun oxfordWordDao(): OxfordWordDao
     abstract fun mostCommonWordDao(): MostCommonWordDao
     abstract fun commonPhraseDao(): CommonPhraseDao
+    abstract fun favoriteWordDao(): FavoriteWordDao
 }
