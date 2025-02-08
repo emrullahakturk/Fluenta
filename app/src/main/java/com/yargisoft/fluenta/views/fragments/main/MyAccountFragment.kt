2769999e7071897a8f29841f523fb16cf784e79e
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-08 22:20:33 +0300

    Update: MyAccountFragment class and xml updated

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/main/MyAccountFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/main/MyAccountFragment.kt
@@ -1,60 +1,78 @@
 package com.yargisoft.fluenta.views.fragments.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.yargisoft.fluenta.R
+import com.yargisoft.fluenta.databinding.FragmentMyAccountBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MyAccountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MyAccountFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+@AndroidEntryPoint
+class MyAccountFragment @Inject constructor() : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private val navOptions = NavOptions.Builder()
+        .setPopUpTo(R.id.main_nav, true) // Main Graph'taki tüm fragmentleri temizler
+        .build()
+
+
+
+    private var _binding: FragmentMyAccountBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_account, container, false)
+    ): View {
+        _binding = FragmentMyAccountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyAccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyAccountFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view,savedInstanceState)
+
+        val user = auth.currentUser
+        if (user != null) {
+            binding.tvName.text = user.displayName ?: "Ad Soyad Belirtilmemiş"
+            binding.tvEmail.text = user.email
+            binding.tvMembership.text = "Üyelik Türü: Ücretsiz"
+        }
+
+        binding.btnLogout.setOnClickListener {
+            auth.signOut()
+            findNavController().navigate(R.id.auth_nav, null, navOptions) // Auth Graph'a yönlendir
+        }
+
+        binding.btnDeleteAccount.setOnClickListener {
+            showDeleteAccountDialog()
+        }
+    }
+
+    private fun showDeleteAccountDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Hesabı Sil")
+            .setMessage("Hesabınızı kalıcı olarak silmek istediğinize emin misiniz?")
+            .setPositiveButton("Evet") { _, _ ->
+                auth.currentUser?.delete()
+                findNavController().navigate(R.id.accountToAuthNav, null, navOptions)
             }
+            .setNegativeButton("İptal", null)
+            .show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
