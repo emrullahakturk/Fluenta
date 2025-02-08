495cd512e25635caec7fc9daa9d3bc1cbfcb5c10
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-08 22:20:07 +0300

    Update: LoginFragment updated. Navigation stack cleared.

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/auth/LoginFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/auth/LoginFragment.kt
@@ -23,7 +23,7 @@ class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
     private val navOptions = NavOptions.Builder()
-        .setPopUpTo(R.id.splashFragment, inclusive = true)
+        .setPopUpTo(R.id.welcomeFragment, inclusive = true)
         .build()
 
     override fun onCreateView(
