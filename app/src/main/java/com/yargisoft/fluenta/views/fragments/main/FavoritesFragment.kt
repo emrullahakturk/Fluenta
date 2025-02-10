ff090526639f2ec14d1b9ec485f627d699b9dad4
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-11 01:44:43 +0300

    Update: FavoritesFragment class updated.

--- app/src/main/java/com/yargisoft/fluenta/views/fragments/main/FavoritesFragment.kt
+++ app/src/main/java/com/yargisoft/fluenta/views/fragments/main/FavoritesFragment.kt
@@ -18,21 +18,26 @@ class FavoritesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
+    ): View {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
 
+        binding.apply {
+            btnOxford.root.setOnClickListener {
+                findNavController().navigate(R.id.favorites_to_oxford_word)
+            }
+            btnOxford.ivIcon.setImageResource(R.drawable.favorite_oxford_words)
 
+            btnC1C2.root.setOnClickListener {
+                findNavController().navigate(R.id.favorites_to_common_word)
+            }
+            btnC1C2.ivIcon.setImageResource(R.drawable.favorite_c1_c2_words)
 
-        binding.btnC1C2.setOnClickListener {
-            findNavController().navigate(R.id.favorites_to_common_word)
+            btnPhrases.root.setOnClickListener {
+                findNavController().navigate(R.id.favorite_to_phrases)
+            }
+            btnPhrases.ivIcon.setImageResource(R.drawable.favorite_common_phrases)
         }
-        binding.btnOxford.setOnClickListener {
-            findNavController().navigate(R.id.favorites_to_oxford_word)
-        }
-        binding.btnPhrases.setOnClickListener {
-            findNavController().navigate(R.id.favorite_to_phrases)
 
-        }
         return binding.root
     }
 }
