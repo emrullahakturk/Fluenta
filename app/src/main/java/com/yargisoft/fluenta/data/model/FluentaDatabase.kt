71d7bd8998508d7d0d797d7b9685f19fb838b6d2
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-23 18:11:38 +0300

    Add: C1C2 Dao added to Fluenta Database

--- app/src/main/java/com/yargisoft/fluenta/data/model/FluentaDatabase.kt
+++ app/src/main/java/com/yargisoft/fluenta/data/model/FluentaDatabase.kt
@@ -3,11 +3,12 @@ package com.yargisoft.fluenta.data.model
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Quote::class, OxfordWord::class,MostCommonWord::class, CommonPhrase::class,Favorite::class], version = 1, exportSchema = false)
+@Database(entities = [Quote::class, OxfordWord::class,MostCommonWord::class, CommonPhrase::class,Favorite::class,C1C2Story::class], version = 1, exportSchema = false)
 abstract class FluentaDatabase : RoomDatabase() {
     abstract fun quoteDao(): QuoteDao
     abstract fun oxfordWordDao(): OxfordWordDao
     abstract fun mostCommonWordDao(): MostCommonWordDao
     abstract fun commonPhraseDao(): CommonPhraseDao
     abstract fun favoriteDao(): FavoriteDao
+    abstract fun c1C2Dao(): C1C2Dao
 }
