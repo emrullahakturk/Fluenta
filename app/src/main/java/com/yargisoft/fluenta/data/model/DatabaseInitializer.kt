82395fd00a237fe129f32dc6ad732e649d18aac8
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-23 18:17:48 +0300

    Update: c1C2Initializer method added to database initializer

--- app/src/main/java/com/yargisoft/fluenta/data/model/DatabaseInitializer.kt
+++ app/src/main/java/com/yargisoft/fluenta/data/model/DatabaseInitializer.kt
@@ -14,6 +14,7 @@ class DatabaseInitializer @Inject constructor(
     private val oxfordWordDao: OxfordWordDao,
     private val mostCommonWordDao: MostCommonWordDao,
     private val commonPhraseDao: CommonPhraseDao,
+    private val c1C2Dao: C1C2Dao,
     ) {
 
     suspend fun quoteInitializer() = withContext(Dispatchers.IO) {
@@ -42,5 +43,12 @@ class DatabaseInitializer @Inject constructor(
         val phrase: List<CommonPhrase> =
             Gson().fromJson(json, object : TypeToken<List<CommonPhrase>>() {}.type)
         commonPhraseDao.insertAll(phrase)
+    }
+
+    suspend fun c1C2Initializer() = withContext(Dispatchers.IO) {
+        val json = context.assets.open("c1_c2_stories.json").bufferedReader().use { it.readText() }
+        val story: List<C1C2Story> =
+            Gson().fromJson(json, object : TypeToken<List<C1C2Story>>() {}.type)
+        c1C2Dao.insertAll(story)
     }
 }
