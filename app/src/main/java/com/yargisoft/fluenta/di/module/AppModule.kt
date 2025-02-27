f197a77bf6b759c3fa8f806108e187b4657a1086
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-27 21:24:25 +0300

    Update: Translator Fragment ui updated & app module updated

--- app/src/main/java/com/yargisoft/fluenta/di/module/AppModule.kt
+++ app/src/main/java/com/yargisoft/fluenta/di/module/AppModule.kt
@@ -22,7 +22,6 @@ object AppModule {
             MainPageMenuItem(R.id.mostCommonWordsFragment, R.drawable.most_common_c1_c2),
             MainPageMenuItem(R.id.mostCommonPhrasesFragment, R.drawable.most_common_phrases),
             MainPageMenuItem(R.id.listenAndLearnFragment, R.drawable.listen_and_learn),
-            MainPageMenuItem(R.id.aiTutorFragment, R.drawable.ai_tutor),
             MainPageMenuItem(R.id.translatorFragment,  R.drawable.translator),
             MainPageMenuItem(R.id.favoritesFragment, R.drawable.favorites),
             MainPageMenuItem(R.id.myAccountFragment, R.drawable.account),
