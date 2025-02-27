e800cf6a7ee7a52ffef460d6331e7ef9dc0d6a07
EmrullahAkturk <akturk.emrullah@hotmail.com>
2025-02-27 21:25:25 +0300

    Update: Translator viewModel updated. Swapping operation added

--- app/src/main/java/com/yargisoft/fluenta/viewmodel/TranslatorViewModel.kt
+++ app/src/main/java/com/yargisoft/fluenta/viewmodel/TranslatorViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.yargisoft.fluenta.data.repository.TranslatorRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,12 +15,33 @@ class TranslatorViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val _translatedText = MutableStateFlow("")
-    val translatedText: StateFlow<String> = _translatedText
+    val translatedText = _translatedText.asStateFlow()
 
-    fun translate(text: String, sourceLang: String, targetLang: String) {
-        viewModelScope.launch {
-            val result = repository.translateText(text, sourceLang, targetLang)
-            _translatedText.value = result
+    private var currentSourceLang = "en"
+    private var currentTargetLang = "tr"
+
+    fun translate(text: String) {
+        if (text.isNotBlank()) {
+            viewModelScope.launch {
+                val translation = repository.translateText(text, currentSourceLang, currentTargetLang)
+                _translatedText.value = translation
+            }
         }
+    }
+
+    fun swapLanguages(text: String) {
+        val tempLang = currentSourceLang
+        currentSourceLang = currentTargetLang
+        currentTargetLang = tempLang
+
+        // Kaynak ve hedef dil isimlerini güncellemek için livedata kullanılabilir.
+        if (currentSourceLang == "en") {
+            _translatedText.value = "English"
+        } else {
+            _translatedText.value = "Türkçe"
+        }
+
+        // Swap sonrası çeviriyi tekrar yap
+        translate(text)
     }
 }
